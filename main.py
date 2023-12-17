1bf64550579c914d624fe840197f6f84a1cda2f2
Rahul Kumar <lightfreemate@gmail.com>
2023-12-17 18:11:53 +0530

    Model trainer added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from textSummerizer.pipeline.steage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummerizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummerizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummerizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from textSummerizer.logging import logger
 
 
@@ -18,6 +20,27 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_trainer = ModelTrainerTrainingPipeline()
+   data_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
