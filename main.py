9936155fc67f11a9017994f3fb9d55753099560e
Rahul Kumar <lightfreemate@gmail.com>
2023-12-17 15:16:27 +0530

    Data Integration added

---
+++ main.py
@@ -0,0 +1,14 @@
+from textSummerizer.pipeline.steage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummerizer.logging import logger
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
