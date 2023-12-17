59050fd1949164a39c1d0576b5df6cdf99bb7f96
Rahul Kumar <lightfreemate@gmail.com>
2023-12-17 16:37:25 +0530

    Validation added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummerizer.pipeline.steage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummerizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummerizer.logging import logger
 
 
@@ -7,6 +8,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
